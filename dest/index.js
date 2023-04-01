5a7b66288474ab133739793940374b65a637899b
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-04-01 08:49:11 +0530

    On branch master  Changes to be committed: 	modified:   dest/index.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	deleted:    public/index.css 	modified:   public/index.html 	deleted:    public/logo.svg 	modified:   src/index.ts

--- dest/index.js
+++ dest/index.js
@@ -1,14 +1,10 @@
 "use strict";
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const canvas_confetti_1 = __importDefault(require("canvas-confetti"));
-canvas_confetti_1.default.create(document.getElementById('canvas'), {
-    resize: true,
-    useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector('#list');
+const form = document.getElementById('#new-task-form');
+const input = document.getElementById('#new-task-title');
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', e => {
+    if ((input === null || input === void 0 ? void 0 : input.value) === '' || (input === null || input === void 0 ? void 0 : input.value) === null)
+        return;
+    input.value;
+});
